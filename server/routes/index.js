df4dff84e3768faf764de7058d07354dc6559b0c
Elena Hadjimina <hadjimina.elena@gmail.com>
2021-05-03 19:53:57 +0800

    detail

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = new express.Router();
 
 router.get('/', (req, res, next) => {
-  res.json();
+  res.json({ type: 'success', data: { title: 'Hello World' } });
 });
 
 module.exports = router;
